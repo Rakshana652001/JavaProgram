f3e9c58103b4eba4e9ff64086200cbb232c66cba
Raks3556 <Raks3556@CSL0699.global.chainsys.com>
2024-04-12 23:04:29 +0530

    methods practice

---
+++ com.chainsys.javaprograms/src/com/chainsys/day4/Addition.java
@@ -0,0 +1,47 @@
+package com.chainsys.day4;
+
+import java.util.Scanner;
+
+public class Addition 
+{
+	public static int addition()
+	{
+		int number1, number2;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter num1 and num2: ");
+		number1 = sc.nextInt();
+		number2 = sc.nextInt();
+		int sum = number1+number2;
+		return sum;
+		
+	}
+	public static int subtraction()
+	{
+		int number1, number2;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter num1 and num2: ");
+		number1 = sc.nextInt();
+		number2 = sc.nextInt();
+		int sub = number1-number2;
+		return sub;
+	}
+	public void multiply()
+	{
+		int number1, number2;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter num1 and num2: ");
+		number1 = sc.nextInt();
+		number2 = sc.nextInt();
+		System.out.println("Multiplication: "+number1*number2);
+	}
+	public static void main(String[] args)
+	{
+		int sum = Addition.addition();
+		System.out.println(sum);
+		int sub = Addition.subtraction();
+		System.out.println(sub);
+		Addition rv = new Addition();
+		rv.multiply();
+	}
+
+}
---
+++ com.chainsys.javaprograms/src/com/chainsys/day4/Methods.java
@@ -0,0 +1,23 @@
+package com.chainsys.day4;
+
+import java.util.Scanner;
+
+public class Methods
+{
+	public static int add()
+	{
+		int a=2, b=4;
+		int c = a+b;
+		return c;
+		
+	}
+
+	public static void main(String[] args) 
+	{
+		
+		int c=Methods.add();
+		System.out.println(c);
+
+	}
+
+}
