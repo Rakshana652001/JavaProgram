57259368f1b88b35dd16d4e49f1f7397fd5143d3
Raks3556 <Raks3556@CSL0699.global.chainsys.com>
2024-04-30 18:01:28 +0530

    insert,delete,update in sql using Java connection

--- com.chainsys.javaprograms/src/com/chainsys/connection/LeaveManagement.java
+++ com.chainsys.javaprograms/src/com/chainsys/connection/LeaveManagement.java
@@ -2,6 +2,7 @@ package com.chainsys.connection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class LeaveManagement {
@@ -9,8 +10,9 @@ public class LeaveManagement {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException 
 	{
 		//LeaveManagement.insert();
-		LeaveManagement.update();
+		//LeaveManagement.update();
 		//LeaveManagement.delete();
+		LeaveManagement.selectFrom();
         
 //        LeaveManagement leaveManagement = new LeaveManagement();
 //        LeaveManagementPojo leaveManagementPojo = new LeaveManagementPojo();
@@ -24,7 +26,7 @@ public class LeaveManagement {
 	{
 		Connection getConnection = JdbcConnection.getConnection();
 		System.out.println(getConnection);
-		String delete = "delete from leaveManagement where employeeID=3;";
+		String delete = "delete from leaveManagement where employeeID=3";
 		PreparedStatement prepareStatement = getConnection.prepareStatement(delete);
 		int save2 = prepareStatement.executeUpdate();
 	System.out.println(save2);
@@ -48,10 +50,23 @@ public class LeaveManagement {
 	public static void update() throws SQLException, ClassNotFoundException
 	{
 		Connection getconnection = JdbcConnection.getConnection();
+		System.out.println(getconnection);
 		String update = "update leaveManagement set employeeID = 3 where employeeName = 'Stark'";
 		PreparedStatement prepareStatement = getconnection.prepareStatement(update);
 		int save1 = prepareStatement.executeUpdate();
 		System.out.println(save1);
+	}
+	
+	public static void selectFrom() throws ClassNotFoundException, SQLException
+	{
+		Connection getconnection = JdbcConnection.getConnection();
+		System.out.println(getconnection);
+		String selectFrom = "Select * from leaveManagement";
+		PreparedStatement prepareStatement = getconnection.prepareStatement(selectFrom);
+		ResultSet save3 = prepareStatement.executeQuery();
+		System.out.println(save3);
+		
+		
 	}
 	
 	
